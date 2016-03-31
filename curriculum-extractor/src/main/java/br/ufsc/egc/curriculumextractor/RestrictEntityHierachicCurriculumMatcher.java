e5f446d4da6101e55fc58c565bd6e2d1ef5c34ab
Mateus Lohn Andriani <mateus.andriani@dataprev.gov.br>
2016-03-31 20:14:17 -0300

    Adição da abordagem utilizando a busca estruturada hierárquica
    -Falta limpeza final da árvore;
    -Classes Tree e Term movidas para o projeto jena-ttl-reader (serão
    movidas para outro JAR posteriormente).

---
+++ curriculum-extractor/src/main/java/br/ufsc/egc/curriculumextractor/RestrictEntityHierachicCurriculumMatcher.java
@@ -0,0 +1,55 @@
+package br.ufsc.egc.curriculumextractor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import br.ufsc.egc.curriculumextractor.model.taxonomy.Term;
+import br.ufsc.egc.curriculumextractor.model.taxonomy.Tree;
+import br.ufsc.egc.dbpedia.reader.service.DBPediaService;
+
+public class RestrictEntityHierachicCurriculumMatcher extends AbstractEntityCurriculumMatcher {
+
+	private static final int LEVELS = 9;
+
+	public void process() {
+
+		EntityImprover improver = new EntityImprover();
+		Map<String, Integer> entitiesCount = improver.getSortedEntitiesMap();
+
+		entitiesCount = getFilteredMap(entitiesCount);
+		
+		List<String> entitiesList = new ArrayList<String>(entitiesCount.keySet());
+
+		DBPediaService dbPediaService = DBPediaService.getInstance();
+		
+		Tree tree = new Tree();
+		
+		for (int index = 0; index < entitiesList.size(); index++) {
+			String entity = entitiesList.get(index);
+			Term hierarchy = dbPediaService.findTree(entity, LEVELS);
+			for (int innerIndex = 0; innerIndex < entitiesList.size(); innerIndex++) {
+				String innerEntity = entitiesList.get(innerIndex);
+				Term result = hierarchy.find(entitiesList.get(innerIndex), true);
+				if (result != null) {
+					while (result.getParent() != null) {
+						addToTree(tree, result.getLabel(), result.getParent().getLabel());
+						result = result.getParent();
+					}
+					addToTree(tree, innerEntity, entity);
+					
+				}
+			}
+		}
+		
+		System.out.println(tree.print());
+		
+		// TODO limpeza da arvore
+		
+	}
+	
+	public static void main(String[] args) {
+		new RestrictEntityHierachicCurriculumMatcher().process();		
+	}
+
+}
