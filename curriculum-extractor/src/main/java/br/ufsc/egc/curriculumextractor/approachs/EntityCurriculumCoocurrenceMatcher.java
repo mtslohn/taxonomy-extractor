faa0d2e062da98ea0291d32be00ae8367a4ea057
Mateus Lohn Andriani <mateus.andriani@dataprev.gov.br>
2016-04-12 20:02:54 -0300

    Adicionadas classes utilitárias para processamento da Tree
    -Adicionada classe TreeJoiner, que faz o join da árvore armazenada em
    results/tree.bin;
    -Adicionada classe TreeOutput, que pega uma árvore em memória e escreve
    no arquivo results/tree.bin;
    -Atualizada classe EntityCurriculumCoocurrenceMatcher para realizar o
    output em arquivo na classe TreeOutput.

--- curriculum-extractor/src/main/java/br/ufsc/egc/curriculumextractor/approachs/EntityCurriculumCoocurrenceMatcher.java
+++ curriculum-extractor/src/main/java/br/ufsc/egc/curriculumextractor/approachs/EntityCurriculumCoocurrenceMatcher.java
@@ -1,5 +1,10 @@
 package br.ufsc.egc.curriculumextractor.approachs;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -14,11 +19,14 @@ import br.ufsc.egc.curriculumextractor.model.EntityPair;
 import br.ufsc.egc.curriculumextractor.model.EntityPairCoocurrence;
 import br.ufsc.egc.curriculumextractor.model.EntityPairCoocurrenceManager;
 import br.ufsc.egc.curriculumextractor.model.taxonomy.Tree;
+import br.ufsc.egc.curriculumextractor.util.TreeOutput;
 import br.ufsc.egc.dbpedia.reader.service.DBPediaService;
 
-public class EntityCurriculumCoocurrenceMatcher extends AbstractEntityCurriculumMatcher {
-	
-	private static final Logger LOGGER = Logger.getLogger(EntityCurriculumCoocurrenceMatcher.class);
+public class EntityCurriculumCoocurrenceMatcher extends
+		AbstractEntityCurriculumMatcher {
+
+	private static final Logger LOGGER = Logger
+			.getLogger(EntityCurriculumCoocurrenceMatcher.class);
 
 	public static void main(String[] args) {
 
@@ -31,7 +39,7 @@ public class EntityCurriculumCoocurrenceMatcher extends AbstractEntityCurriculum
 
 		Map<Integer, String> curriculumMap = curriculumListReader.read();
 		List<String> entities = new ArrayList<String>(entitiesCount.keySet());
-		
+
 		List<CurriculumCorrelation> correlations = new ArrayList<CurriculumCorrelation>();
 
 		for (Integer curriculumKey : curriculumMap.keySet()) {
@@ -39,9 +47,9 @@ public class EntityCurriculumCoocurrenceMatcher extends AbstractEntityCurriculum
 			String curriculum = curriculumMap.get(curriculumKey);
 
 			for (int indexOuter = 0; indexOuter < entities.size(); indexOuter++) {
-				
+
 				String entityOuter = entities.get(indexOuter);
-				
+
 				if (curriculum.contains(entityOuter)) {
 
 					for (int indexInner = indexOuter + 1; indexInner < entities
@@ -56,51 +64,53 @@ public class EntityCurriculumCoocurrenceMatcher extends AbstractEntityCurriculum
 							pair.setEntity2(entityInner);
 							correlation.getPairs().add(pair);
 						}
-						
+
 						if (!correlation.getPairs().isEmpty()) {
 							correlations.add(correlation);
 						}
-						
+
 					}
 				}
 
 			}
 
 		}
-		
+
 		EntityPairCoocurrenceManager manager = new EntityPairCoocurrenceManager();
-		
+
 		for (int index = 0; index < correlations.size(); index++) {
 			if (index % 1000 == 0) {
 				LOGGER.info("Processando " + index);
 			}
 			CurriculumCorrelation correlation = correlations.get(index);
-			for (EntityPair pair: correlation.getPairs()) {
+			for (EntityPair pair : correlation.getPairs()) {
 				manager.addPair(pair.getEntity1(), pair.getEntity2());
 			}
 		}
-		
-		// ja tenho as relacoes e a frequencia delas... agora eh hora de montar a arvore
+
+		// ja tenho as relacoes e a frequencia delas... agora eh hora de montar
+		// a arvore
 		// TODO colocar na tree as relacoes descobertas e validadas na DBPedia
-		
+
 		DBPediaService dbPediaService = DBPediaService.getInstance();
-		
+
 		Tree tree = new Tree();
-		
+
 		for (EntityPair pair : manager.getPairsCoocurrence().keySet()) {
-			List<String> broadersEntity1 = dbPediaService.findBroaderConcepts(pair.getEntity1());
+			List<String> broadersEntity1 = dbPediaService
+					.findBroaderConcepts(pair.getEntity1());
 			if (broadersEntity1.contains(pair.getEntity2())) {
 				tree.addToTree(pair.getEntity2(), pair.getEntity1());
 			}
-			List<String> broadersEntity2 = dbPediaService.findBroaderConcepts(pair.getEntity2());
+			List<String> broadersEntity2 = dbPediaService
+					.findBroaderConcepts(pair.getEntity2());
 			if (broadersEntity2.contains(pair.getEntity1())) {
 				tree.addToTree(pair.getEntity1(), pair.getEntity2());
 			}
 		}
 		
-		tree.join();
-		
-		System.out.println(tree.print());
+		TreeOutput treeOutput = new TreeOutput();
+		treeOutput.write(tree);
 		
 	}
 
---
+++ curriculum-extractor/src/main/java/br/ufsc/egc/curriculumextractor/util/TreeJoiner.java
@@ -0,0 +1,48 @@
+package br.ufsc.egc.curriculumextractor.util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import org.apache.log4j.Logger;
+
+import br.ufsc.egc.curriculumextractor.approachs.AbstractEntityCurriculumMatcher;
+import br.ufsc.egc.curriculumextractor.model.taxonomy.Tree;
+
+public class TreeJoiner extends AbstractEntityCurriculumMatcher {
+	
+	private static final Logger LOGGER = Logger.getLogger(TreeJoiner.class);
+	
+	public static final String BINARY_INPUT_FILE = "results/tree.bin";
+
+	public void process() {
+		
+		FileInputStream fileInput;
+		try {
+			fileInput = new FileInputStream(BINARY_INPUT_FILE);
+			ObjectInputStream objectInput = new ObjectInputStream(fileInput);
+			Tree tree = (Tree) objectInput.readObject();
+			
+			tree.join();
+			
+			System.out.println(tree.print());
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		new TreeJoiner().process();
+	}
+	
+}
